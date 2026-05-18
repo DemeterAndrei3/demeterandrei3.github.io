6866ead7502233ac5497ae4ebd80d7e6e1521142
DemeterAndrei3 <demeterandrei2004@gmail.com>
2026-05-18 19:55:53 +0300

    Add City model with schema definition

--- Test_18_05_2026/models/City.js
+++ Test_18_05_2026/models/City.js
@@ -1,1 +1,14 @@
+// models/City.js
+const mongoose = require('mongoose');
+
+const citySchema = new mongoose.Schema({
+  city: { type: String, required: true },
+  lat: { type: Number, required: true },
+  lng: { type: Number, required: true },
+  country: { type: String, required: true },
+  iso: { type: String, required: true },
+  capital: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model('City', citySchema, 'cities');
 
